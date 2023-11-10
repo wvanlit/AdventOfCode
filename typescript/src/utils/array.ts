88433ad053d9e10dd2bb6352a61ee2774cb9eff3
Wessel van Lit <wessel@litdev.nl>
2023-11-10 20:32:53 +0100

    TS update array helpers

--- typescript/src/utils/array.ts
+++ typescript/src/utils/array.ts
@@ -1,5 +1,3 @@
-import { A } from "@mobily/ts-belt";
-
 /**
  * Creates an array of arrays, each containing a "window" of elements from the input array.
  * @param arr - The input array.
@@ -7,14 +5,20 @@ import { A } from "@mobily/ts-belt";
  * @returns An array of windows.
  */
 export function window<T>(arr: T[], size: number): T[][] {
-  const range = A.range(0, A.length(arr) - size);
-  const sliced = range.map((start) => A.slice(arr, start, start + size));
+  const result: T[][] = [];
+  for (let i = 0; i <= arr.length - size; i++) {
+    result.push(arr.slice(i, i + size));
+  }
 
-  return sliced as T[][];
+  return result;
 }
 
 export function max(arr: number[]): number {
   return arr.reduce((p, c) => (c > p ? c : p));
+}
+
+export function sum(arr: number[]): number {
+  return arr.reduce((p, c) => p + c, 0);
 }
 
 export function sortMutable(arr: number[]): number[] {
@@ -28,4 +32,27 @@ export function sortMutable(arr: number[]): number[] {
 export function sort(arr: number[]): number[] {
   const arrCopy = [...arr];
   return arrCopy.sort((a, b) => a - b);
+}
+
+export function permute<T>(arr: T[], n: number): T[][] {
+  if (n > arr.length) {
+    throw new Error("N cannot be larger than the array length");
+  }
+
+  function helper(startIndex: number, temp: T[]): void {
+    if (temp.length === n) {
+      result.push([...temp]);
+      return;
+    }
+
+    for (let i = startIndex; i < arr.length; i++) {
+      temp.push(arr[i]);
+      helper(i + 1, temp);
+      temp.pop();
+    }
+  }
+
+  const result: T[][] = [];
+  helper(0, []);
+  return result;
 }
