78be924a86c3cc5283c86e762b9d87f868720745
Wessel van Lit <wessel@litdev.nl>
2023-11-12 15:41:12 +0100

    C# 2020/13

--- csharp/AdventOfCode/Solution.cs
+++ csharp/AdventOfCode/Solution.cs
@@ -24,6 +24,7 @@ public record Answer(string Value)
 {
     public static implicit operator Answer(string value) => new(value);
     public static implicit operator Answer(int value) => new(value.ToString());
+    public static implicit operator Answer(long value) => new(value.ToString());
 
     public static implicit operator string(Answer answer) => answer.Value;
 }
