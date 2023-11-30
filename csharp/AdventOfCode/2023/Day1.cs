d7ba2ed9f63f9ff33cf43ffade31308e7d7d5a48
Wessel van Lit <wessel@litdev.nl>
2023-11-30 18:51:24 +0100

    C# - 2023/1 prep

---
+++ csharp/AdventOfCode/2023/Day1.cs
@@ -0,0 +1,15 @@
+namespace AdventOfCode._2023;
+
+[EventIdentifier(year: 2023, day: 1)]
+public class Day1 : Solution
+{
+    public override Answer Part1(string input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Answer Part2(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
