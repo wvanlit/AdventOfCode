1188376c9220411b84b1a5298e802944dc804abc
Wessel van Lit <wessel@litdev.nl>
2023-12-04 08:11:14 +0100

    Nim 2023/4 - Dirty solution w/ memo

--- nim/2023/day4.nim
+++ nim/2023/day4.nim
@@ -1,5 +1,68 @@
 include "../prelude"
+import math, algorithm
 
-let input = readInput(2023, 4, test=false).strip
+type Card = tuple[id: int, winning: seq[int], numbers: seq[int]]
 
-echo input
+func parse(line: string): Card =
+    let ticket = line.split(":")
+    let parts = ticket[1].split("|")
+
+    let id = ticket[0].replace("Card", "").strip.parseInt
+    let winning = parts[0].strip.split(" ").filterIt(not it.isEmptyOrWhitespace).mapIt(it.parseInt)
+    let numbers = parts[1].strip.split(" ").filterIt(not it.isEmptyOrWhitespace).mapIt(it.parseInt)
+    
+    (id, winning, numbers)
+
+func hasMatchingNumbers(card: Card): int =
+    card.numbers.filterIt(card.winning.contains(it)).len
+
+func calcPoints(card: Card): int =
+    let hits = hasMatchingNumbers(card)
+    if hits == 0: 
+        0 
+    else: 
+        2 ^ (hits - 1)
+
+func calculateResultingCards(card: Card, cards: seq[Card], cache: TableRef[int, CountTable[int]]): CountTable[int] =
+    result = initCountTable[int]()
+    result.inc(card.id)
+
+    if cache.hasKey(card.id):
+        return cache[card.id]
+
+    let points = hasMatchingNumbers(card)
+    if points > 0:
+        let next = (1..points).toSeq.mapIt(card.id + it)
+        
+        var counts = newSeq[CountTable[int]]()
+        for n in next:
+            counts.add(calculateResultingCards(cards[n - 1], cards, cache))
+        for t in counts:
+            for (k, v) in t.pairs:
+                result.inc(k, v)
+
+    cache[card.id] = result
+    
+
+proc calculateWonCards(cards: seq[Card]): CountTable[int] =
+    result = initCountTable[int]()
+
+    var cache = newTable[int, CountTable[int]]()
+    var results = cards.mapIt(calculateResultingCards(it, cards, cache))
+
+    for t in results:
+        for (k, v) in t.pairs:
+            result.inc(k, v)
+    
+
+let cards = readInput(2023, 4, test=false).strip.splitLines.mapIt(parse(it))
+
+echo "Part 1: ", cards.mapIt(calcPoints(it)).sum
+
+let cardsWon = calculateWonCards(cards)
+var sum = 0
+for (k, v) in cardsWon.pairs:
+    echo k, ": ", v
+    sum += v
+
+echo "Part 2: ", sum
