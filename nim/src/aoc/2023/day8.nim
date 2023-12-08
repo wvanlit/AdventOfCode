cc0e53145e3c1f06947855e523d6982f089a4bcb
Wessel van Lit <wessel@litdev.nl>
2023-12-08 23:24:46 +0100

    Nim 2023/8 - LCM

--- nim/src/aoc/2023/day8.nim
+++ nim/src/aoc/2023/day8.nim
@@ -1,5 +1,5 @@
 import "../utils/io_utils"
-import sequtils, strutils, tables, math
+import sequtils, strutils, tables, math, sugar
 
 type Path = seq[char]
 type Node = tuple[id: string, left: string, right: string]
@@ -20,11 +20,11 @@ func parse(input: string): tuple[path: Path, nodes: Nodes] =
 
         result.nodes[id] = (id, left, right)
 
-proc part1(path: Path, nodes: Nodes): int =
-    var currentNode = nodes["AAA"]
+proc part1(path: Path, nodes: Nodes, start: string = "AAA", exitCondition: string -> bool): int =
+    var currentNode = nodes[start]
     var steps = 0
 
-    while currentNode.id != "ZZZ":
+    while not exitCondition(currentNode.id):
         let dir = path[steps mod path.len] # Prevents index out of bounds
         currentNode = if dir == 'L': nodes[currentNode.left] else: nodes[currentNode.right]
         inc(steps)
@@ -33,21 +33,16 @@ proc part1(path: Path, nodes: Nodes): int =
 
 proc part2(path: Path, nodes: Nodes): int =
     var currentNodes = nodes.values.toSeq.filterIt(it.id[^1] == 'A')
-    var steps = 0
-
-    while not currentNodes.allIt(it.id.contains("Z")):
-        let dir = path[steps mod path.len] # Prevents index out of bounds
-        currentNodes = currentNodes.mapIt(if dir == 'L': nodes[it.left] else: nodes[it.right])
-        inc(steps)
-
-    return steps
+    var indices = currentNodes.mapIt(part1(path, nodes, it.id, x => x[^1] == 'Z'))
+    
+    return lcm(indices)
 
 
 proc main() =
     let input = readInput(2023, 8, test = false).strip
     let map = parse(input)
 
-    echo "Part 1: ", part1(map.path, map.nodes)
+    echo "Part 1: ", part1(map.path, map.nodes, "AAA", x => x == "ZZZ")
     echo "Part 2: ", part2(map.path, map.nodes)
 
 main()
