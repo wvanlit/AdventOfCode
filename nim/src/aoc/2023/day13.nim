e9faabe5bd4bff53f87b7c4f950d8bec379593d1
Wessel van Lit <wessel@litdev.nl>
2023-12-13 18:28:34 +0100

    Nim 2023/13 - P1

--- nim/src/aoc/2023/day13.nim
+++ nim/src/aoc/2023/day13.nim
@@ -7,36 +7,60 @@ func parse(line: string): Grid[char] =
     result = initGrid[char](len(gridLines[0]), len(gridLines))
     for y, line in gridLines:
         for x, c in line:
-            result.setValue(x, y, c) 
+            result.setValue(x, y, if c == '#': '1' else: '0') 
 
-func checkVerticalSymmetry(grid: Grid[char]): bool =
-    let pairs = (0..grid.width).toSeq.window(2).mapIt((it[0], it[1]))
-    let check = ((1..4).toSeq, (5..9).toSeq.reversed)
+func generateSymmetryChecks(length: int): seq[tuple[a: Slice[int], b: Slice[int]]] =
+    let pairs = (0..length-1).toSeq.window(2).mapIt((it[0], it[1]))
 
-    for i, (x1, x2) in check[0].zip(check[1]):
-        # TODO: check if this is correct
+    result = @[]
+    for i, (x1, x2) in pairs:
+        let half = min(x1, (length - 1) - x2) # Only check until either is out of bounds
+        let r1 = (x1-half..x1)
+        let r2 = (x2..x2+half)
+        result.add((r1, r2))
 
+func checkSymmetry(grid: Grid[char], indices: tuple[a: Slice[int], b: Slice[int]], checkRow = false): bool =
+    let (r1, r2) = indices
+    let s1 = r1.toSeq
+    let s2 = r2.toSeq.reversed()
 
+    let b1 = s1.mapIt(if checkRow: grid.row(it) else: grid.col(it)).flatten.join("")
+    let b2 = s2.mapIt(if checkRow: grid.row(it) else: grid.col(it)).flatten.join("")
 
-func checkHorizontalSymmetry(grid: Grid[char]): bool =
-    let pairs = (0..grid.height).toSeq.window(2).mapIt((it[0], it[1]))
-
-func checkSymmetry(grid: Grid[char]): bool =
-    if checkVerticalSymmetry(grid):
+    if b1 == b2: 
         return true
-    elif checkHorizontalSymmetry(grid):
-        return true
-    else:
-        return false
+    return false
+
+func checkVerticalSymmetry(grid: Grid[char]): tuple[a: int, b: int] =
+    let pairs = generateSymmetryChecks(grid.width)
+    
+    for p in pairs:
+        if grid.checkSymmetry(p, checkRow=false): 
+            return (p.a.b, p.b.a)
+    return (0, 0)
+
+func checkHorizontalSymmetry(grid: Grid[char]) : tuple[a: int, b: int] =
+    let pairs = generateSymmetryChecks(grid.height)
+    for p in pairs:
+        if grid.checkSymmetry(p, checkRow=true): 
+            return (p.a.b, p.b.a)
+    return (0, 0)
+
+func checkSingleSymmetry(grid: Grid[char]): int =
+    let vertical = checkVerticalSymmetry(grid)
+    let horizontal = checkHorizontalSymmetry(grid)
+
+    assert(not (vertical.a == 0 and vertical.b == 0 and horizontal.a == 0 and horizontal.b == 0), "No symmetry found")
+
+    if vertical != (0,0): # Vertical symmetry
+        return vertical.a + 1 # Columns to the left of the symmetry line
+    else: # Horizontal symmetry
+        return (horizontal.a + 1) * 100# Rows above the symmetry line
 
 proc main() =
-    let input = readInput(2023, 13, test=true).strip.split("\n\n").map(parse)
+    let input = readInput(2023, 13, test=false).strip.split("\n\n").map(parse)
     
-    for i in input:
-        i.printGrid()
-        echo i.checkSymmetry()
-
-    echo "Part 1: ", -1
+    echo "Part 1: ", input.map(checkSingleSymmetry).sum
     echo "Part 2: ", -1
 
 main()
