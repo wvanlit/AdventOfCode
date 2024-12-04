baf1c065d0413f539f130cf457b8ccf609e1f12a
Wessel van Lit <wessel@litdev.nl>
2024-12-04 20:05:54 +0100

    Clean up unused langs

--- processing/src/lib/components/random_color.js
+++
@@ -1,6 +0,0 @@
-export function getRandomColor() {
-  const colors = ["#fff", "#aaa", "#f00", "#0f0", "#00f"];
-
-  const index = Math.floor(Math.random() * colors.length);
-  return colors[index];
-}
--- processing/src/main.js
+++
@@ -1,5 +0,0 @@
-import { loadFiles, loadSketch } from './lib/files'
-
-loadFiles();
-
-document.getElementById("load-sketch").onclick = loadSketch
