b3bc08263f4fbf30334d65a9055a30f39b3a2af1
Wessel van Lit <wessel@litdev.nl>
2024-12-10 20:27:14 +0100

    2024 Day 9 - This is ugly code but it does work

--- csharp/AoC.2024/Day9.cs
+++ csharp/AoC.2024/Day9.cs
@@ -1,5 +1,7 @@
-﻿using System.Text;
+﻿using System.Collections;
+using System.Text;
 using AoC.Shared;
+using MoreLinq;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -8,28 +10,47 @@ namespace AoC._2024;
 public class Day9(ITestOutputHelper outputHelper) :
     SolutionBase(year: 2024, day: 9, runTest: false, outputHelper: outputHelper)
 {
-    private record struct Block(long Index, int Len, long? Id)
+    private record DiskObject(long StartsAtIndex, long BlockLength)
     {
-        public bool IsFile => Id.HasValue;
+        public virtual string ToShortString() => $"DO({BlockLength}) @ {StartsAtIndex}";
+
+        public record File(long StartsAtIndex, long BlockLength, long Id) : DiskObject(StartsAtIndex, BlockLength)
+        {
+            public override string ToString() =>
+                string.Join("", Enumerable.Range(0, (int)BlockLength).Select(_ => Id.ToString()));
+
+            public override string ToShortString() => $"{Id}({BlockLength}) @ {StartsAtIndex}";
+            
+            public Empty ToEmpty() => new(StartsAtIndex, BlockLength);
+        }
+
+        public record Empty(long StartsAtIndex, long BlockLength) : DiskObject(StartsAtIndex, BlockLength)
+        {
+            public override string ToString() =>
+                string.Join("", Enumerable.Range(0, (int)BlockLength).Select(_ => "."));
+
+            public override string ToShortString() => $"Empty({BlockLength}) @ {StartsAtIndex}";
+        }
     }
 
-    private static List<Block> Parse(string input)
+    private static List<DiskObject> Parse(string input)
     {
-        var chars = input.ToCharArray();
-        var result = new List<Block>();
+        var digits = input.ToCharArray().Select(c => c.ToString()).Select(int.Parse).ToArray();
+        var result = new List<DiskObject>();
         var id = 0;
         var fileIndex = 0;
 
-        for (var i = 0; i < chars.Length; i++)
+        for (var i = 0; i < digits.Length; i++)
         {
-            var len = int.Parse(chars[i].ToString());
+            var len = digits[i];
+
             if (i % 2 == 0) // Even equals file
             {
-                result.Add(new(fileIndex, len, id++));
+                result.Add(new DiskObject.File(fileIndex, len, id++));
             }
             else // Odd equals empty space
             {
-                result.Add(new(fileIndex, len, null));
+                result.Add(new DiskObject.Empty(fileIndex, len));
             }
 
             fileIndex += len;
@@ -38,92 +59,238 @@ public class Day9(ITestOutputHelper outputHelper) :
         return result;
     }
 
-    private string CurrentState(
-        IEnumerable<Block> files,
-        IEnumerable<Block> emptySpaces,
-        IEnumerable<Block> movedBlocks)
-    {
-        var result = files
-            .Concat(movedBlocks)
-            .Concat(emptySpaces)
-            .OrderBy(b => b.Index)
-            .ToList();
-
-        var sb = new StringBuilder();
-        foreach (var r in result)
+    private void PrintDiskMap(IEnumerable<DiskObject.File> files, IEnumerable<DiskObject.Empty> emptySpace,
+        bool includeIndex = false) =>
+        WriteIfTest(() =>
         {
-            if (r.IsFile)
-                sb.Append(r.Id!.Value.ToString()[0], r.Len);
-            else
-                sb.Append('.', r.Len);
+            var list = new List<DiskObject>();
+
+            list.AddRange(files);
+            list.AddRange(emptySpace);
+            list.Sort((a, b) => a.StartsAtIndex.CompareTo(b.StartsAtIndex));
+
+            var result = string.Join("", list.Select(d => d.ToString()));
+            if (!includeIndex) return result;
+
+            return "R: " + result + "\nI: " + string.Join("", result.Select((c, i) => i.ToString().Last()));
+        });
+
+    private List<DiskObject.File> CompactPerBlock(List<DiskObject> diskMap)
+    {
+        var files = new List<DiskObject.File>(diskMap.OfType<DiskObject.File>());
+        var emptyBlocks = new Queue<DiskObject.Empty>(diskMap.OfType<DiskObject.Empty>());
+
+        while (emptyBlocks.TryDequeue(out var empty))
+        {
+            // Empty block is after all files
+            if (files.All(f => f.StartsAtIndex < empty.StartsAtIndex)) continue;
+
+            WriteIfTest(() => $"\nEmpty: {empty.ToShortString()}");
+
+            while (empty.BlockLength > 0)
+            {
+                if (files.All(f => f.StartsAtIndex < empty.StartsAtIndex)) break;
+
+                PrintDiskMap(files, emptyBlocks.Prepend(empty), true);
+
+                var file = files.Last();
+
+                if (file.BlockLength == empty.BlockLength) // file fills exactly
+                {
+                    var prepend = file with { StartsAtIndex = empty.StartsAtIndex };
+
+                    WriteIfTest(() => $"Exact: {prepend.ToShortString()}");
+
+                    files = files.SkipLast(1).Prepend(prepend).ToList();
+                }
+                else if (file.BlockLength < empty.BlockLength) // file underfills
+                {
+                    var prepend = file with { StartsAtIndex = empty.StartsAtIndex };
+
+                    WriteIfTest(() => $"Underfill: {prepend.ToShortString()}");
+
+                    files = files.SkipLast(1).Prepend(prepend).ToList();
+                }
+                else if (file.BlockLength > empty.BlockLength) // file overfills
+                {
+                    var diff = file.BlockLength - empty.BlockLength;
+
+                    var prepend = file with { StartsAtIndex = empty.StartsAtIndex, BlockLength = empty.BlockLength };
+                    var append = file with { BlockLength = diff };
+
+                    WriteIfTest(() => $"Overfill: {prepend.ToShortString()} & {append.ToShortString()}");
+
+                    files = files.SkipLast(1).Prepend(prepend).Append(append).ToList();
+                }
+                else
+                {
+                    Assert.True(false, "Invalid State");
+                }
+
+                empty = empty with
+                {
+                    BlockLength = empty.BlockLength - file.BlockLength,
+                    StartsAtIndex = empty.StartsAtIndex + file.BlockLength
+                };
+            }
         }
 
-        return sb.ToString();
+        Assert.Empty(emptyBlocks);
+
+        return files.OrderBy(f => f.StartsAtIndex).ToList();
+    }
+
+    private long Checksum(IEnumerable<DiskObject.File> files)
+    {
+        var expanded = files.SelectMany(f => Enumerable.Range(0, (int)f.BlockLength).Select(_ => f.Id)).ToArray();
+
+        WriteIfTest(() => string.Join("", expanded));
+
+        return expanded.Select((id, index) => id * (long)index).Sum();
     }
 
     public override async Task<Answer> Part1(string input)
     {
-        var blocks = Parse(input);
+        WriteIfTest(input);
 
-        var files = new Stack<Block>(blocks.Where(b => b.IsFile));
-        var emptySpaces = new Queue<Block>(blocks.Where(b => !b.IsFile));
-        var movedBlocks = new List<Block>();
-        
-        OutputHelper.WriteLine($"Blocks: {blocks.Count}");
-        OutputHelper.WriteLine($"Files: {files.Count}");
-        OutputHelper.WriteLine($"Empty spaces: {emptySpaces.Count}");
+        var diskMap = Parse(input);
 
-        while (emptySpaces.TryDequeue(out var nextEmpty))
+        var compactedFiles = CompactPerBlock(diskMap);
+
+        PrintDiskMap(compactedFiles, []);
+
+        return Checksum(compactedFiles);
+    }
+
+    [Fact]
+    public async Task SampleDiskMap()
+    {
+        var input = "12345";
+
+        WriteIfTest(input);
+
+        var diskMap = Parse(input);
+
+        WriteIfTest("\nCompacting:");
+        var compactedFiles = CompactPerBlock(diskMap);
+
+        var expanded = string.Join("", compactedFiles.Select(d => d.ToString()));
+
+        WriteIfTest("\nExpanded:");
+        WriteIfTest(expanded);
+        foreach (var file in compactedFiles)
         {
-            if (!files.Any(f => f.Index >= nextEmpty.Index))
+            WriteIfTest(file.ToShortString());
+        }
+
+
+        Assert.Equal("022111222", expanded);
+    }
+
+    private List<DiskObject> CompactPerFile(List<DiskObject> diskMap)
+    {
+        var files = new List<DiskObject.File>(diskMap.OfType<DiskObject.File>());
+        var emptyBlocks = new Queue<DiskObject.Empty>(diskMap.OfType<DiskObject.Empty>());
+        var emptyBlocksToKeep = new List<DiskObject.Empty>();
+
+        while (emptyBlocks.TryDequeue(out var empty))
+        {
+            if (empty.BlockLength == 0) continue;
+            
+            // Empty block is after all files
+            if (files.All(f => f.StartsAtIndex < empty.StartsAtIndex))
             {
+                emptyBlocksToKeep.Add(empty);
                 continue;
             }
-            
-            var newIndex = nextEmpty.Index;
-            var remainingLength = nextEmpty.Len;
-            while (remainingLength > 0)
+
+            WriteIfTest(() => $"\nEmpty: {empty.ToShortString()}");
+
+            while (empty.BlockLength > 0)
             {
-                nextEmpty = nextEmpty with { Len = remainingLength, Index = newIndex};
+                if (files.All(f => f.StartsAtIndex < empty.StartsAtIndex))
+                {
+                    // No file fits in the (remaining) empty block
+                    emptyBlocksToKeep.Add(empty);
+                    break;
+                }
+
+                PrintDiskMap(files, emptyBlocks.Prepend(empty), true);
+
+                var file = files
+                    .Where(f => f.StartsAtIndex > empty.StartsAtIndex)
+                    .LastOrDefault(f => f.BlockLength <= empty.BlockLength);
+
+
+                if (file == default)
+                {
+                    // No file fits in the (remaining) empty block
+                    emptyBlocksToKeep.Add(empty);
+                    break;
+                }
                 
-                WriteIfTest(() => CurrentState(files, emptySpaces.Prepend(nextEmpty), movedBlocks));
+                files.Remove(file);
 
-                var currentFile = files.Pop();
-                if (currentFile.Len <= remainingLength)
+                if (file.BlockLength == empty.BlockLength) // file fills exactly
                 {
-                    movedBlocks.Add(currentFile with { Index = newIndex });
+                    var prepend = file with { StartsAtIndex = empty.StartsAtIndex };
+                    WriteIfTest(() => $"Exact: {prepend.ToShortString()}");
+
+                    files = files.Prepend(prepend).ToList();
+                    emptyBlocksToKeep.Add(file.ToEmpty());
                 }
-                else
+                else if (file.BlockLength < empty.BlockLength) // file underfills
                 {
-                    var fills = currentFile.Len - remainingLength;
+                    var prepend = file with { StartsAtIndex = empty.StartsAtIndex };
+                    WriteIfTest(() => $"Underfill: {prepend.ToShortString()}");
 
-                    movedBlocks.Add(currentFile with { Index = newIndex, Len = remainingLength });
-                    files.Push(currentFile with { Len = fills });
+                    files = files.Prepend(prepend).ToList();
+                    emptyBlocksToKeep.Add(file.ToEmpty());
+                }
+                else // Overfill is not possible in Part 2
+                {
+                    Assert.True(false, "Invalid State");
                 }
 
-                newIndex += currentFile.Len; // Overshooting is fine, we'll reset on the next iteration
-                remainingLength -= currentFile.Len;
+                empty = empty with
+                {
+                    BlockLength = empty.BlockLength - file.BlockLength,
+                    StartsAtIndex = empty.StartsAtIndex + file.BlockLength
+                };
             }
         }
-        
-        WriteIfTest(CurrentState(files, emptySpaces, movedBlocks));
 
-        Assert.Empty(emptySpaces);
+        Assert.Empty(emptyBlocks);
 
-        var flatList = files
-            .Concat(movedBlocks)
-            .OrderBy(b => b.Index)
-            .SelectMany(block => Enumerable.Range(0, block.Len).Select(i => block.Id!.Value));
-        
-        WriteIfTest(string.Join("", flatList));
-        
-        var checksum = flatList.Select((b, idx) => b * idx).Sum();
-        
-        return checksum;
+        return files.Cast<DiskObject>()
+            .Concat(emptyBlocksToKeep)
+            .OrderBy(f => f.StartsAtIndex)
+            .ToList();
+    }
+    
+    private long Checksum(IEnumerable<DiskObject> objects)
+    {
+        var expanded = objects.SelectMany(f => Enumerable.Range(0, (int)f.BlockLength).Select(_ => f)).ToArray();
+
+        WriteIfTest(() => string.Join("\n", expanded.Select(s => s.ToShortString())));
+
+        return expanded
+            .Select((obj, index) => (obj, index))
+            .Where(t => t.obj is DiskObject.File)
+            .Select(t => ((DiskObject.File)t.obj).Id * t.index)
+            .Sum();
     }
 
     public override async Task<Answer> Part2(string input)
     {
-        return Answer.Failed;
+        WriteIfTest(input);
+
+        var diskMap = Parse(input);
+
+        var compactedFiles = CompactPerFile(diskMap);
+
+        PrintDiskMap(compactedFiles.OfType<DiskObject.File>(), compactedFiles.OfType<DiskObject.Empty>());
+
+        return Checksum(compactedFiles);
     }
 }
